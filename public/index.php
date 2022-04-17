8baf99602f62858ee16f65b778b59857df3d0f02
tiago <tiagolemespalhano@gmail.com>
2022-04-17 03:55:50 -0300

    Home melhorada

--- public/index.php
+++ public/index.php
@@ -21,24 +21,74 @@ define("TITULO", 'Home');
 
 <?php include 'includes/header.php' ?>
 
+<?php
+
+// livros
+$sql = "select count(id) as total from livros";
+
+$result = mysqli_query($conn, $sql);
+
+$data = mysqli_fetch_assoc($result);
+
+$livros = $data['total'];
+
+// autores
+$sql = "select count(id) as total from autores";
+
+$result = mysqli_query($conn, $sql);
+
+$data = mysqli_fetch_assoc($result);
+
+$autores = $data['total'];
+
+// leitores
+$sql = "select count(id) as total from leitores";
+
+$result = mysqli_query($conn, $sql);
+
+$data = mysqli_fetch_assoc($result);
+
+$leitores = $data['total'];
+
+// leituras
+$sql = "select count(livro_id) as total from leituras";
+
+$result = mysqli_query($conn, $sql);
+
+$data = mysqli_fetch_assoc($result);
+
+$leituras = $data['total'];
+
+?>
+
   
 <div class="container mt-5">
   <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
+
+    <div class="col-md-6 col-lg-3">
+      <h3><?= $livros ?> livros</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
+
+    <div class="col-md-6 col-lg-3">
+      <h3><?= $autores ?> autores</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
+
+    <div class="col-md-6 col-lg-3">
+      <h3><?= $leitores ?> leitores</h3>        
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
+
+    <div class="col-md-6 col-lg-3">
+      <h3><?= $leituras ?> leituras</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+
   </div>
 </div>
 
